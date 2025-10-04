e86e7a48172ca4f6b225d4707d6b2ab324f52d93
bhumi0987 <bhumibhardwaj267@gmail.com>
2025-10-04 21:37:26 +0530

    Update routes.ts

--- api/routes.ts
+++ api/routes.ts
@@ -1,3 +1,14 @@
+
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === 'GET') {
+    res.status(200).json({ message: 'Hello from API' });
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
+
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
