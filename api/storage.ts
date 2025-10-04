e9a7bfc756d9831c02ad2fa2bd1ba35162bac639
bhumi0987 <bhumibhardwaj267@gmail.com>
2025-10-04 21:37:41 +0530

    Update storage.ts

--- api/storage.ts
+++ api/storage.ts
@@ -1,3 +1,13 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === 'GET') {
+    res.status(200).json({ message: 'Hello from API' });
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
+
 import { type User, type InsertUser } from "@shared/schema";
 import { randomUUID } from "crypto";
 
